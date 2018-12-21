93fdd5748450293b0f25b87f9e94eefb7e763e4a
Rogelio Zamudio <yeyozaz@gmail.com>
2018-12-21 00:59:52 -0600

    created Home component skeleton, handleFormSubmit will send get request to API for weather data

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 
-class Home extends Component {
+export default class Home extends Component {
   state ={
-    city: ""
+    city: ''
   }
-
 
   handleInputchange = event => {
     const{ name, value } = event.target;
@@ -21,9 +20,32 @@ class Home extends Component {
   render() {
     return (
       <div>
-        
+        <form>
+          <divtn className='form-group'>
+            <label htmlFor='Weather'>
+              <strong>Weather</strong>
+            </label>
+            <input
+              className='form-control'
+              id='City'
+              type='text'
+              value={this.state.city}
+              placeholder='Enter a city'
+              onChange={this.handleInputchange}
+              required
+            />
+          </div>
+          <div className='pull-right'>
+            <button
+              onClick={handleFormSubmit}
+              type='submit'
+              className='btn btn-lg btn-danger float-right'
+            >
+              Search
+            </button>
+          </div>
+        </form>
       </div>
     )
   }
 }
-export default Home
