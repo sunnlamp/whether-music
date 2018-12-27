6db224baf14adb3f62f9a3c0a180aa89cd2c5e60
Rogelio Zamudio <yeyozaz@gmail.com>
2018-12-26 22:35:00 -0600

    added enzyme and enzyme adapter to write Home tets, enzyme still needs to be configured properly;

---
+++ client/src/components/Home.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import Home from './Home'
+import { isThisHour } from 'date-fns';
+
+
+describe('Home', () => {
+  let wrapper
+  beforeEach(() => wrapper = shallow(<Home />))
+
+  it('should render correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should render a div', () => {
+    expect(wrapper.find('div').length).toEqual(1)
+  })
+})
