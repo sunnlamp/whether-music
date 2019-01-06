367d55397da56ccd53a7f1ecf8bff8c420d645fc
Rogelio Zamudio <yeyozaz@gmail.com>
2019-01-05 18:59:30 -0600

    created MusicList and ListItem skeletons

---
+++ client/src/components/MusicList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ListItem'
+
+const MusicList = () => {
+  return (
+    <div className="music-list-container">
+      <List Item/>
+    </div>
+  )
+}
+
+export default MusicList
