57537274db8efe812bdd171eac0ca52f1417f825
Rogelio Zamudio <yeyozaz@gmail.com>
2019-01-07 10:10:58 -0600

    added additional skeleton work

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const ListItem = () => {
-
+  <div>
+    <div>
+      <img src="" alt=""/>
+      Image
+    </div>
+    <div>
+      <div>
+        Track Name
+      </div>
+      <div>
+        Artist Name
+      </div>
+    </div>
+  </div>
 }
 
 export default ListItem
