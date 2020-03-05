1b5ca669c7f14ba0a5b818e31eb784a47f272d81
Rogelio Zamudio <yeyozaz@gmail.com>
2020-03-04 21:22:04 -0600

    added color sweep to music item container

--- client/src/components/MusicBox.js
+++ client/src/components/MusicBox.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 import '../styles/index.css'
 
 const MusicBox = ({ id, name, preview_url, artist_name, album_name, album_art }) => (
-  <div key={id}>
-    <span>{name}</span>
-    <a href={preview_url}></a>
-    <span>{artist_name}</span>
-    <h3>{album_name}</h3>
-    <img src={album_art} alt={album_name}/>
-  </div>
+  <section key={id} className='card hvr-sweep-to-top'>
+    <header className='headline'>
+      <img src={album_art} alt={album_name} />
+    </header>
+    <div className='article'>
+      <h2>{name}</h2>
+      <h3>{artist_name}</h3>
+      <hr/>
+    </div>
+  </section>
 )
 
 export default MusicBox
