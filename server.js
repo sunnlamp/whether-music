06c6ab8cac92f765eba298ca6890938142af2e44
Rogelio Zamudio <yeyozaz@gmail.com>
2020-06-21 19:44:49 -0500

    modified server.js

--- server.js
+++ server.js
@@ -19,6 +19,17 @@ var weatherData = []
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const setSpotifyAccessToken = () => {
   let tokenPromise = null;
 
