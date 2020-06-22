edb3d239ef9ba1ee6d0647ecb244070d3a14a5f8
Rogelio Zamudio <yeyozaz@gmail.com>
2020-06-21 20:10:00 -0500

    modified server

--- server.js
+++ server.js
@@ -135,8 +135,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
